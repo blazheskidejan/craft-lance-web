90a4d731219a66480a4f59a30d1ba410157e0095
d3jan <dblazeski96@gmail.com>
2018-04-21 12:11:20 +0200

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import TestComp from './screens/Landing.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <TestComp/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/screens/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TestComp extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt soluta quaerat doloribus deserunt laudantium exercitationem odit, assumenda voluptas aspernatur cupiditate sit maiores repellat, hic modi nisi iure, reprehenderit ipsa vero.</p>
+            </div>
+        );
+    }
+}
+
+export default TestComp;
